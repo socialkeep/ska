565c00112df2e5f40eda094730bdd90d20fa8c0c
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2019-03-06 12:27:32 -0800

    added 1 test

---
+++ test/api/index.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/server/server');
+
+/**
+ *  Testing GET content endpoints
+ */
+describe('GET /pending', function(){
+  it('Respond with an array of objects containing a list of all pending posts.', function(done){
+    request(app)
+      .get('/pending')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
+
+/**
+ *  Testing POST content endpoint
+ */
+
+
+
+
+/**
+ *  Testing POST admin login endpoint
+ */
+
+ 
+
