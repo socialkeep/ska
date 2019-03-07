46100bc509fe24873ec66a542be65d0b0b595d59
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-03-07 14:51:51 -0800

    more styling

--- src/client/components/SubmissionContainerComponents/ContactInfoComponent.jsx
+++ src/client/components/SubmissionContainerComponents/ContactInfoComponent.jsx
@@ -7,17 +7,18 @@ const ContactInfoComponent = (props) => {
 
     return (
       <div className="SubmissionComponents" className="PostAndPhotoComponent">
-          <h1>SocialKeep</h1>
-          <h2>Social Media Submissions</h2>
           <h3>Please enter your contact details, so we can share your social media post!</h3>
 
           <input className="inputContactDetails" id="firstName" placeholder='First Name' onChange={props.handleTextChange} value={props.firstName} type="text"/>
           <br></br>
+          <br></br>
 
           <input className="inputContactDetails" id="lastName" placeholder='Last Name' onChange={props.handleChange} value={props.lastName} type="text"/>
           <br></br>
+          <br></br>
 
           <input className="inputContactDetails" id="email" placeholder='Email Address' onChange={props.handleChange} value={props.email} type="text"/>
+          <br></br>
           <br></br>
 
           <button className="standardBlueButton"><Link to={`/submission/socialmediadetails`}>Continue</Link></button>
